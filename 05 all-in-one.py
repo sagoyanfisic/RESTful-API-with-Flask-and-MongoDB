41ad52b6b6471430540fd3b4332c14e5f2e32529
Mahmudul Hasan <sreejon07@gmail.com>
2018-04-05 18:03:20 +0600

    new file added

--- 05 all-in-one.py
+++ 05 all-in-one.py
@@ -1,6 +1,16 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,g
+import sqlite3
 
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
 
 userData = {}
 userData['1'] = {
@@ -16,6 +26,8 @@ userData['2'] = {
     "password" : "22"
 }
 
+print(type(userData))
+
 @app.route('/users', methods=['POST'])
 def addOne():
     size = len(userData) + 1
@@ -30,6 +42,9 @@ def addOne():
     if "" == request.json['firstName'] or "" == request.json['lastName'] or "" == request.json['password']:
         return jsonify({"Status" : "Not Acceptable"}), 406
     return jsonify({"id" : userData[userId]['id']}), 201
+
+c.close()
+conn.close()
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
