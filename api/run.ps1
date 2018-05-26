a406e3e0e85e56727e00dd7e58771911436d4fcf
Mahmudul Hasan <sreejon07@gmail.com>
2018-05-26 13:48:54 +0600

    master updating

--- api/run.ps1
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --name test_api_container -it -d -p 8080:8080 flask_api:latest
--- clean.ps1
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose down
-docker-compose stop
-docker rmi -f app_api:latest
-docker rmi -f app_db:latest
